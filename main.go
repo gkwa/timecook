7e280cfd28f8766c23f0fbfa29c5f944d34e2e2a
Taylor Monacelli <taylormonacelli@gmail.com>
2022-11-21 20:10:45 -0800

    Initial

---
+++ main.go
@@ -0,0 +1,19 @@
+package timecook
+
+import (
+	humanize "github.com/dustin/go-humanize"
+	"github.com/minio/minio/pkg/disk"
+)
+
+func printUsage(path string) error {
+	di, err := disk.GetInfo(path)
+	if err != nil {
+		return err
+	}
+	percentage := (float64(di.Total-di.Free) / float64(di.Total)) * 100
+	fmt.Printf("%s of %s disk space used (%0.2f%%)\n",
+		humanize.Bytes(di.Total-di.Free),
+		humanize.Bytes(di.Total),
+		percentage,
+	)
+}
